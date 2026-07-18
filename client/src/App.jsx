951a8fdb21605adcf4c9f1e1b893788337f2a8aa
myadavaramshravan-ship-it <myadavaramshravan@gmail.com>
2026-07-18 12:29:26 +0530

    Fix React router deployment

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,25 @@
-import AppRoutes from "./routes/AppRoutes";
+import { Routes, Route } from "react-router-dom";
+
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
 
 
 function App(){
 
-    return <AppRoutes />;
+  return (
+    <Routes>
 
+      <Route path="/" element={<Login />} />
+
+      <Route path="/login" element={<Login />} />
+
+      <Route path="/register" element={<Register />} />
+
+      <Route path="/dashboard" element={<Dashboard />} />
+
+    </Routes>
+  );
 }
-
 
 export default App;
