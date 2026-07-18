508aa57775b30efda11f3abb7f9b8d59f88297a8
myadavaramshravan-ship-it <myadavaramshravan@gmail.com>
2026-07-18 12:35:16 +0530

    Fix react router root route

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,36 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-import {
-    BrowserRouter
-} from "react-router-dom";
-
-
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 
-import AuthProvider from "./context/AuthContext.jsx";
-
-import "./App.css";
-
-
-ReactDOM.createRoot(
-    document.getElementById("root")
-).render(
-
-
-    <BrowserRouter>
-
-
-        <AuthProvider>
-
-
-            <App />
-
-
-        </AuthProvider>
-
-
-    </BrowserRouter>
-
-
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
