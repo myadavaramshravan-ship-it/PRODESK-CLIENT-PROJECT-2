5b40856088786e9eb5e014f883a6f67a25afc4e3
myadavaramshravan-ship-it <myadavaramshravan@gmail.com>
2026-07-18 12:31:43 +0530

    Fix default React route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -10,13 +10,25 @@ function App(){
   return (
     <Routes>
 
-      <Route path="/" element={<Login />} />
+      <Route 
+        path="/" 
+        element={<Navigate to="/login" replace />}
+      />
 
-      <Route path="/login" element={<Login />} />
+      <Route 
+        path="/login" 
+        element={<Login />}
+      />
 
-      <Route path="/register" element={<Register />} />
+      <Route 
+        path="/register" 
+        element={<Register />}
+      />
 
-      <Route path="/dashboard" element={<Dashboard />} />
+      <Route 
+        path="/dashboard" 
+        element={<Dashboard />}
+      />
 
     </Routes>
   );
